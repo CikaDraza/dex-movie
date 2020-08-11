94c5f4f63d13429139813070e9e00653f7672b72
Milan Drazic <drazic.milan@gmail.com>
2020-08-11 21:37:45 +0200

    Updates

---
+++ precache-manifest.cb4d738c798f247f524bcbb2605915a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f221769505e5311c20779a3719934a4",
+    "url": "/dex-movie/index.html"
+  },
+  {
+    "revision": "972f7a7e826d899ff32e",
+    "url": "/dex-movie/static/css/main.3d61c51a.chunk.css"
+  },
+  {
+    "revision": "165f1e85f5997ddae2e6",
+    "url": "/dex-movie/static/js/2.4f2e8d0f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dex-movie/static/js/2.4f2e8d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972f7a7e826d899ff32e",
+    "url": "/dex-movie/static/js/main.12d62db4.chunk.js"
+  },
+  {
+    "revision": "e32d66fe593f14948a3d",
+    "url": "/dex-movie/static/js/runtime-main.7ca7cbb3.js"
+  },
+  {
+    "revision": "c0dc36e89868ef05720d82d27680ce8f",
+    "url": "/dex-movie/static/media/bgd.c0dc36e8.jpg"
+  },
+  {
+    "revision": "2c8b3b9c99f9815291cad367408b27d8",
+    "url": "/dex-movie/static/media/dex-logo.2c8b3b9c.png"
+  }
+]);
